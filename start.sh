46d3cc2f2870b5b67379e5ffea7588b37400f4de
JuManji <20031287+JuManjey@users.noreply.github.com>
2021-11-11 15:17:24 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,10 @@ cd Invoke-Obfuscation
 pwsh
 Import-Module ./Invoke-Obfuscation.psd1
 Invoke-Obfuscation
-set scriptblock Write-Host "Hello World!"
-
-
-
+set scriptblock Write-Host "Hello World!" # set scriptpath http://192.0.0.1/scriptname.ps1
+TOKEN
+STRING
+# 1 + 2
+#result -> Write-Host (("{0}{1}"-f 'H','ello ')+'W'+("{0}{1}"-f 'o','rld!'))
+#check in poweshell -> Write-Host (("{0}{1}"-f 'H','ello ')+'W'+("{0}{1}"-f 'o','rld!')) - > Hello World!
+#
